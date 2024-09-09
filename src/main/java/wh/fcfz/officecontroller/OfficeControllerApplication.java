519475d4d024514c76a9a97a5ce6694e4f66c27c
Driver-Tu <3109702282@qq.com>
2024-09-09 11:31:03 +0800

    首次提交

---
+++ src/main/java/wh/fcfz/officecontroller/OfficeControllerApplication.java
@@ -0,0 +1,11 @@
+package wh.fcfz.officecontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeControllerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OfficeControllerApplication.class, args);
+    }
+}
---
+++ src/test/java/wh/fcfz/officecontroller/OfficeControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package wh.fcfz.officecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
