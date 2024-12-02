2633042d944a7d3d494258923fc0c8cc034ae806
尹嘉豪 <2424389790@qq.com>
2024-12-02 17:25:15 +0800

    查询表单数据 -v4.1.3

---
+++ src/main/java/wh/fcfz/officecontroller/all/bean/Vo/FormValueVo.java
@@ -0,0 +1,61 @@
+package wh.fcfz.officecontroller.all.bean.Vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FormValueVo {
+
+    /**
+     * 表单通用表主键
+     */
+    private Integer formId;
+
+    /**
+     * 表单名称
+     */
+    private String formName;
+
+    /**
+     * 表单类型 id 外键
+     */
+    private Integer templateId;
+
+    /**
+     * 表单路由地址
+     */
+    private String formRouter;
+
+    /**
+     * 是否开启定时发送（0否 1是）
+     */
+    private String isScheduled;
+
+    /**
+     * 发送时间
+     */
+    private Date sendTime;
+
+    /**
+     * 表单状态（0 草稿，1 已提交）
+     */
+    private String status;
+
+    /**
+     * 更新时间
+     */
+    private Date updatedTime;
+
+    /**
+     * 提交时间
+     */
+    private Date submitTime;
+
+    private List<FormFieldValueVo> formFieldValues;
+}
